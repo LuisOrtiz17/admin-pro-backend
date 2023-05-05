6a362fbfbca89488493fc0a11399aad4a776f8a7
LuisOrtiz17 <zamudioluisortiz@gmail.com>
2023-05-05 13:24:08 -0600

    Fin Sección 12 - Google Sign-in

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const Usuario = require('../models/usuario.model');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async( req, res = response) => {
 
@@ -49,6 +50,53 @@ const login = async( req, res = response) => {
 
 }
 
+const loginGoogle = async( req, res = response) => {
+
+    try {
+
+        const {email, name, picture} = await googleVerify(req.body.token);
+
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if( !usuarioDB ){
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            });
+        }else {
+            usuario = usuarioDB;
+            //usuario.password = '@@'  //Para bloquear que el usuario se siga logeando con su contraseña registrada, quedaría su acceso solo por google sign-in
+            usuario.google = true
+        }
+
+        //Guardar Usuario
+        await usuario.save();
+
+        //Generar jwt - TOKEN
+        const token = await generarJWT(usuario.id);
+
+        res.status(200).json({
+            ok: true,
+            email, name, picture,
+            token
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Token de google no es correcto'
+        });
+        
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    loginGoogle
 }
